143b0fd3b7ca040784074a297883d0d185c0be68
Martijn Meijers <b.m.meijers@tudelft.nl>
2014-06-11 13:47:54 +0200

    Back to development: 1.0.5

--- src/oseq/__init__.py
+++ src/oseq/__init__.py
@@ -1,7 +1,7 @@
 """An Ordered Sequence of objects where duplicates are allowed.
 """
 
-__version__ = '1.0.4'
+__version__ = '1.0.5.dev0'
 __license__ = 'MIT License'
 __author__ = 'Martijn Meijers'
 __creation_date__ = '2009-03-26'
